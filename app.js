a0d6a46f353d527788683c39ae76f60f14c43575
leandrop <leandro.p.n96@gmail.com>
2021-08-15 22:25:43 -0300

    feat: :sparkles: implementado o socket.io ao server NodeJs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var app = require('./config/server')
 
 //Definição da porta do app
-app.listen(8001, function(){
+var server = app.listen(8001, function(){
     console.log("Servidor rodando na porta 8001")
 })
+
+require('socket.io')(server)
