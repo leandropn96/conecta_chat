28ccbb0cd8a980582075c2f9471ebdd6c9659ccd
leandrop <leandro.p.n96@gmail.com>
2021-08-18 00:00:19 -0300

    feat: :sparkles: Adicionado coversas realtime no chate. Com websocket

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 var app = require('./config/server')
 
 //Definição da porta do app
-var server = app.listen(8001, function(){
+var server = app.listen(8002, function () {
     console.log("Servidor rodando na porta 8001")
 })
 
-require('socket.io')(server)
+var io = require('socket.io')(server)
+
+//Setando io como uma variavel de ambiente
+app.set('io', io)
+
+//criar conexão com websocket.io
+io.on('connection', function (socket) {
+    console.log("conexão aberta")
+    socket.on('disconnect', function () {
+        console.log("o usuario desconectou")
+    })
+    socket.on('enviarMensagem', function (data) {
+        console.log("teste")
+        socket.emit('addUsuario', { usuario: data.apelido, msg: data.msg })
+        socket.broadcast.emit('addUsuario', { usuario: data.apelido, msg: data.msg })
+    })
+})
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,15 +1,16 @@
-var {body, validationResult} = require('express-validator')
-module.exports.chat = function(application, req, res){
+var { body, validationResult } = require('express-validator')
+module.exports.chat = function (application, req, res) {
     res.render('chat')
 }
 
-module.exports.includeChat = function(application, req, res){
+module.exports.includeChat = function (application, req, res) {
     var errors = validationResult(req).isEmpty()
-    if(!errors){
-        res.render('index', {validacao: validationResult(req).errors});
+    if (!errors) {
+        res.render('index', { validacao: validationResult(req).errors });
         return
     }
-    return
-    
-    res.render('chat')
+
+    application.get('io').emit('addUsuario', { usuario: req.body.apelido, msg: 'Acabou de entra no chat' })
+
+    res.render('chat', { dadosForm: req.body })
 }
